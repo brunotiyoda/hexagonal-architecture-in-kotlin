9cc6b8b37fc18a460664063a4d85e25e03c13a86
Bruno Yudi Tiyoda <bruno_tiyoda@hotmail.com>
2021-02-11 13:50:34 -0300

    unresolved reference

--- persistence/src/main/kotlin/com/example/hexarq/adapters/ExampleAdapter.kt
+++ persistence/src/main/kotlin/com/example/hexarq/adapters/ExampleAdapter.kt
@@ -5,9 +5,9 @@ import com.example.hexarq.entities.ExampleEntity
 import com.example.hexarq.entities.toExampleDomain
 import com.example.hexarq.ports.out.ExamplePersistence
 import com.example.hexarq.repositories.ExampleRepository
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Component
 
-@Service
+@Component
 class ExampleAdapter(
     private val exampleRepository: ExampleRepository
 ) : ExamplePersistence {

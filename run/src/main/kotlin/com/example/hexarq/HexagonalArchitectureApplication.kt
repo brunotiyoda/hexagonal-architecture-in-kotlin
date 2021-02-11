a5594530d5c4141b60532fb1d5ab3ef0b3b31cb0
Bruno Yudi Tiyoda <bruno_tiyoda@hotmail.com>
2021-02-11 13:50:34 -0300

    unresolved reference

--- run/src/main/kotlin/com/example/hexarq/HexagonalArchitectureApplication.kt
+++ run/src/main/kotlin/com/example/hexarq/HexagonalArchitectureApplication.kt
@@ -3,7 +3,7 @@ package com.example.hexarq
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["com.example.hexarq"])
 open class HexagonalArchitectureApplication
 
 fun main(args: Array<String>) {
